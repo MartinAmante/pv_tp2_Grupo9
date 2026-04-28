385f6e6b94dddd7ef7ee8befee295386df81c90e
Llampa Alejandro Matias Salomón <matias.asllampa@gmail.com>
2026-04-28 09:52:54 -0300

    avance de ejercicios 4 y 8 al tp2 - Matias Llampa

---
+++ js/ejercicio4.js
@@ -0,0 +1,12 @@
+const base = 10;
+const altura = 6;
+
+const area = (base, altura) => {
+    console.log(`El área de un rectángulo es ${base * altura} cm²`);
+}
+
+const perimetro = (base, altura) => {
+    console.log(`el perímetro de un rectángulo es ${2 * (base + altura)} cm`)
+}
+area (base, altura)
+perimetro(base, altura)
