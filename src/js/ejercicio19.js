720fd90ccbfbc57d60dbf4e8e406a230df5b88cd
Llampa Alejandro Matias Salomón <matias.asllampa@gmail.com>
2026-05-05 15:59:33 -0300

    Entrega del punto 19 finalizado - Matias Llampa

---
+++ src/js/ejercicio19.js
@@ -0,0 +1,10 @@
+import { cambiarTexto } from "../services/funciones19.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const boton = document.querySelector("#btn-cambiar");
+
+    boton.addEventListener("click", () => {
+        cambiarTexto("#texto-51", "51 La felicidad de quienes quieren ser populares depende de los demás, la de los que buscan placer cambia con el humor, que no podemos controlar. Pero la de los sabios viene de sus propias acciones.");
+        cambiarTexto("#texto-52", "52 Las cosas no nos afectan por sí solas, sino por lo que pensamos sobre ellas.");
+    });
+});
---
+++ src/services/funciones19.js
@@ -0,0 +1,4 @@
+export function cambiarTexto(selector, nuevoTexto) {
+    const elemento = document.querySelector(selector);
+    elemento.textContent = nuevoTexto;
+}
